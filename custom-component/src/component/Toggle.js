546698a2556c75a947aca635e4cc04f74fc782b1
grappe96 <w00k0805@naver.com>
2022-01-28 18:02:03 +0900

    Create Toggle Component
    /src/component/Toggle.js : toggle component
    /src/css/common.css : common style css
    /src/css/toggle.css : toggle component style css

--- custom-component/src/component/Toggle.js
+++ custom-component/src/component/Toggle.js
@@ -1,5 +1,33 @@
+import '../css/common.css';
+import '../css/toggle.css';
+import { useState } from 'react';
+
 function Toggle() {
-  return <div> This is Toggle</div>;
+  const [isChecked, setIsChecked] = useState(false);
+  const checkEventHandler = ({ target }) => {
+    setIsChecked(!isChecked);
+  };
+
+  return (
+    <>
+      <div className="title">Toggle</div>
+      <div className="tiggle-body">
+        <label class="toggle-button">
+          <input
+            type="checkbox"
+            checked={isChecked}
+            onChange={(e) => checkEventHandler(e)}
+          />
+          <span class="toggle-switch" />
+        </label>
+        {isChecked ? (
+          <div className="notice">Toggle Switch ON</div>
+        ) : (
+          <div className="notice">Toggle Switch OFF</div>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default Toggle;
